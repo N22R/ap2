6516771c645155929ccc9ca5462f83d77449c518
N22R <nazarmelnyk.nzr@gmail.com>
2025-06-06 12:28:32 +0300

    Add files via upload

--- src/sum.py
+++ src/sum.py
@@ -1,1 +1,14 @@
+class TreeNode:
+    def __init__(self, value=0, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
 
+
+def sum_of_depths(root: TreeNode) -> int:
+    def calculate_depth(node, depth):
+        if node is None:
+            return 0
+        return depth + calculate_depth(node.left, depth + 1) + calculate_depth(node.right, depth + 1)
+
+    return calculate_depth(root, 0)
