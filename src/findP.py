0de2f2d0af234fe325c1d157595c1a4d4a535801
N22R <nazarmelnyk.nzr@gmail.com>
2025-06-06 12:24:58 +0300

    Add files via upload

--- src/findP.py
+++ src/findP.py
@@ -1,1 +1,47 @@
+def merge_sort(arr):
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        left_half = arr[:mid]
+        right_half = arr[mid:]
 
+        merge_sort(left_half)
+        merge_sort(right_half)
+
+        i = j = k = 0
+
+        while i < len(left_half) and j < len(right_half):
+            if left_half[i] < right_half[j]:
+                arr[k] = left_half[i]
+                i += 1
+            else:
+                arr[k] = right_half[j]
+                j += 1
+            k += 1
+
+        while i < len(left_half):
+            arr[k] = left_half[i]
+            i += 1
+            k += 1
+
+        while j < len(right_half):
+            arr[k] = right_half[j]
+            j += 1
+            k += 1
+
+def find_3nums(nums, num_P):
+    nums = list(set(nums))
+    merge_sort(nums)
+    n = len(nums)
+
+    for i in range(n - 2):
+        left, right = i + 1, n - 1
+        while left < right:
+            current_sum = nums[i] + nums[left] + nums[right]
+            if current_sum == num_P:
+                return "Такі числа є"
+            if current_sum < num_P:
+                left += 1
+            else:
+                right -= 1
+
+    return "Таких чисел немає"
